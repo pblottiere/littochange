37cd131c1f526400bb2bcec00fcd1b92d50747a0
Blottiere Paul <blottiere.paul@gmail.com>
2020-10-10 01:02:01 +0200

    Rename and clean

--- provider/provider.py
+++ provider/provider.py
@@ -19,23 +19,10 @@ class LittoDynProvider(QgsProcessingProvider):
         self.addAlgorithm(ChangeDetectionAlgorithm())
 
     def id(self, *args, **kwargs):
-        """The ID of your plugin, used for identifying the provider.
-
-        This string should be a unique, short, character only string,
-        eg "qgis" or "gdal". This string should not be localised.
-        """
-        return 'yourplugin'
+        return 'littodyn'
 
     def name(self, *args, **kwargs):
-        """The human friendly name of your plugin in Processing.
-
-        This string should be as short as possible (e.g. "Lastools", not
-        "Lastools version 1.0.1 64-bit") and localised.
-        """
-        return self.tr('Your plugin')
+        return self.tr('LittoDyn')
 
     def icon(self):
-        """Should return a QIcon which is used for your provider inside
-        the Processing toolbox.
-        """
         return QgsProcessingProvider.icon(self)
