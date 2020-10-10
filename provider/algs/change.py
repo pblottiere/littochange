e69827ea12de592fac1bc4b3def0b5563d962654
Blottiere Paul <blottiere.paul@gmail.com>
2020-10-10 03:46:21 +0200

    Add custom wrappers

--- provider/algs/change.py
+++ provider/algs/change.py
@@ -8,21 +8,132 @@ __email__ = "blottiere.paul@gmail.com"
 __license__ = "GPLv3"
 
 from qgis.PyQt.QtCore import QCoreApplication
+from qgis.PyQt.QtWidgets import QComboBox, QLineEdit
+
+from qgis import processing
 from qgis.core import (QgsProject,
+                       QgsMapLayer,
+                       QgsMessageLog,
                        QgsProcessing,
                        QgsFeatureSink,
                        QgsProcessingException,
                        QgsProcessingAlgorithm,
-                       QgsProcessingParameterRasterLayer,
+                       QgsProcessingParameterDefinition,
                        QgsProcessingParameterFeatureSource,
                        QgsProcessingParameterFeatureSink)
-from qgis import processing
+
+from processing.gui.wrappers import WidgetWrapper
+
+
+class LittoDynRasterComboBoxWrapper(WidgetWrapper):
+
+    def __init__(self, param, dialog, row=0, col=0, **kwargs):
+        super().__init__(param, dialog, row, col, **kwargs)
+
+    def createWidget(self):
+        widget = QComboBox()
+        widget.addItem("")
+        widget.currentIndexChanged.connect(self.event)
+
+        project = QgsProject.instance()
+        for id, layer in project.mapLayers().items():
+            if layer.type() != QgsMapLayer.RasterLayer:
+                continue
+
+            if not layer.temporalProperties().isActive():
+                continue
+
+            widget.addItem(id)
+
+        return widget
+
+    def value(self):
+        return self.widget.currentText()
+
+    def layer(self):
+        return QgsProject.instance().mapLayer(self.widget.currentText())
+
+    def event(self):
+        w_raster_1 = None
+        w_raster_2 = None
+        w_date = None
+
+        mw = self.dialog.mainWidget()
+        if not mw:
+            return
+
+        for parameter in self.dialog.algorithm().parameterDefinitions():
+            name = parameter.name()
+
+            if name == 'INFO_DATE':
+                w_date = mw.wrappers[name]
+
+            if name == 'INPUT_RASTER_1':
+                w_raster_1 = mw.wrappers[name]
+
+            if name == 'INPUT_RASTER_2':
+                w_raster_2 = mw.wrappers[name]
+
+        if w_raster_1 and w_raster_2 and w_date:
+            raster_1_layer = w_raster_1.layer()
+            raster_2_layer = w_raster_2.layer()
+
+            if not raster_1_layer or not raster_2_layer:
+                return
+
+            raster_1_prop = raster_1_layer.temporalProperties()
+            raster_2_prop = raster_2_layer.temporalProperties()
+
+            begin_1 = raster_1_prop.fixedTemporalRange().begin()
+            begin_2 = raster_2_prop.fixedTemporalRange().begin()
+
+            days = begin_1.daysTo(begin_2)
+            w_date.setValue(str(days))
+
+
+class LittoDynRasterParameter(QgsProcessingParameterDefinition):
+
+    def __init__(self, name='', description='', options=[], default=None, isSource=False,
+                 multiple=False, optional=False):
+
+        QgsProcessingParameterDefinition.__init__(self, name, description, default, optional)
+
+        self.setMetadata({
+            'widget_wrapper': {
+                'class': LittoDynRasterComboBoxWrapper}})
+
+
+class LittoDynValueWrapper(WidgetWrapper):
+
+    def __init__(self, param, dialog, row=0, col=0, **kwargs):
+        super().__init__(param, dialog, row, col, **kwargs)
+
+    def createWidget(self):
+        self.widget = QLineEdit()
+        self.widget.setReadOnly(True)
+        return self.widget
+
+    def setValue(self, val):
+        self.widget.setText(val)
+
+
+class LittoDynDateParameter(QgsProcessingParameterDefinition):
+
+    def __init__(self, name='', description='', options=[], default=None, isSource=False,
+                 multiple=False, optional=False):
+
+        QgsProcessingParameterDefinition.__init__(self, name, description, default, optional)
+
+        self.setMetadata({
+            'widget_wrapper': {
+                'class': LittoDynValueWrapper}})
 
 
 class ChangeDetectionAlgorithm(QgsProcessingAlgorithm):
     INPUT_EXTENT = 'INPUT_EXTENT'
     INPUT_RASTER_1 = 'INPUT_RASTER_1'
     INPUT_RASTER_2 = 'INPUT_RASTER_2'
+    INFO_DATE = 'INFO_DATE'
     OUTPUT = 'OUTPUT'
 
     def tr(self, string):
@@ -50,30 +161,25 @@ class ChangeDetectionAlgorithm(QgsProcessingAlgorithm):
         )
 
         self.addParameter(
-            QgsProcessingParameterRasterLayer(
+            LittoDynRasterParameter(
                 self.INPUT_RASTER_1,
-                self.tr('Raster 1'),
-                [QgsProcessing.TypeRaster]
+                self.tr('Raster 1')
             )
         )
 
         self.addParameter(
-            QgsProcessingParameterRasterLayer(
+            LittoDynRasterParameter(
                 self.INPUT_RASTER_2,
-                self.tr('Raster 2'),
-                [QgsProcessing.TypeRaster]
+                self.tr('Raster 2')
             )
         )
 
-        # We add a feature sink in which to store our processed features (this
-        # usually takes the form of a newly created vector layer when the
-        # algorithm is run in QGIS).
-        # self.addParameter(
-        #     QgsProcessingParameterFeatureSink(
-        #         self.OUTPUT,
-        #         self.tr('Output layer')
-        #     )
-        # )
+        self.addParameter(
+            LittoDynDateParameter(
+                self.INFO_DATE,
+                self.tr('Days between rasters')
+            )
+        )
 
     def processAlgorithm(self, parameters, context, feedback):
         extent = self.parameterAsSource(
@@ -82,17 +188,19 @@ class ChangeDetectionAlgorithm(QgsProcessingAlgorithm):
             context
         )
 
-        raster_1 = self.parameterAsRasterLayer(
+        raster_1_id = self.parameterAsString(
             parameters,
             self.INPUT_RASTER_1,
             context
         )
+        raster_1 = QgsProject.instance().mapLayer(raster_1_id)
 
-        raster_2 = self.parameterAsRasterLayer(
+        raster_2_id = self.parameterAsString(
             parameters,
             self.INPUT_RASTER_2,
             context
         )
+        raster_2 = QgsProject.instance().mapLayer(raster_2_id)
 
         name = "{}_{}".format(raster_1.name(), raster_2.name())
         root = QgsProject.instance().layerTreeRoot()
