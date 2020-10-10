b9de7e6be3ed7c00f24b12765a680bca2ae99949
Blottiere Paul <blottiere.paul@gmail.com>
2020-10-10 05:11:24 +0200

    Add layers to group

--- provider/algs/change.py
+++ provider/algs/change.py
@@ -13,9 +13,13 @@ from qgis.PyQt.QtWidgets import QComboBox, QLineEdit
 from qgis import processing
 from qgis.core import (QgsProject,
                        QgsMapLayer,
+                       QgsGeometry,
+                       QgsFields,
                        QgsMessageLog,
+                       QgsVectorLayer,
                        QgsProcessing,
                        QgsFeatureSink,
+                       QgsProcessingUtils,
                        QgsProcessingException,
                        QgsProcessingAlgorithm,
                        QgsProcessingParameterDefinition,
@@ -116,6 +120,9 @@ class LittoDynValueWrapper(WidgetWrapper):
     def setValue(self, val):
         self.widget.setText(val)
 
+    def value(self):
+        return 0
+
 
 class LittoDynDateParameter(QgsProcessingParameterDefinition):
 
@@ -134,7 +141,7 @@ class ChangeDetectionAlgorithm(QgsProcessingAlgorithm):
     INPUT_RASTER_1 = 'INPUT_RASTER_1'
     INPUT_RASTER_2 = 'INPUT_RASTER_2'
     INFO_DATE = 'INFO_DATE'
-    OUTPUT = 'OUTPUT'
+    OUTPUT_BUFFER = 'OUTPUT_BUFFER'
 
     def tr(self, string):
         return QCoreApplication.translate('Processing', string)
@@ -182,7 +189,7 @@ class ChangeDetectionAlgorithm(QgsProcessingAlgorithm):
         )
 
     def processAlgorithm(self, parameters, context, feedback):
-        extent = self.parameterAsSource(
+        extent = self.parameterAsVectorLayer(
             parameters,
             self.INPUT_EXTENT,
             context
@@ -204,82 +211,31 @@ class ChangeDetectionAlgorithm(QgsProcessingAlgorithm):
 
         name = "{}_{}".format(raster_1.name(), raster_2.name())
         root = QgsProject.instance().layerTreeRoot()
-        group = root.addGroup(name)
+        self.group = root.addGroup(name)
+
+        vl = extent.clone()
+        vl.setName("Extent")
+        QgsProject.instance().addMapLayer(vl)
+        self.group.addLayer(vl)
+
+        vl = QgsVectorLayer("Polygon", "Bufferized extent", "memory")
+        vl.setCrs(extent.crs())
+        vl.startEditing()
+        for feature in extent.getFeatures():
+            geom = feature.geometry()
+            buffer = geom.buffer(0.001, 4, QgsGeometry.CapFlat, QgsGeometry.JoinStyleMiter, 100)
+            feature.setGeometry(buffer)
+            feature.setFields(QgsFields())
+            vl.addFeature(feature)
+        vl.updateExtents()
+        vl.commitChanges()
+
+        QgsProject.instance().addMapLayer(vl)
+        self.group.addLayer(vl)
+
+        vl = QgsVectorLayer("Polygon", "TODO", "memory")
+        vl.setCrs(extent.crs())
+        QgsProject.instance().addMapLayer(vl)
+        self.group.addLayer(vl)
 
         return {}
-
-        # Retrieve the feature source and sink. The 'dest_id' variable is used
-        # to uniquely identify the feature sink, and must be included in the
-        # dictionary returned by the processAlgorithm function.
-        source = self.parameterAsSource(
-            parameters,
-            self.INPUT,
-            context
-        )
-
-        # If source was not found, throw an exception to indicate that the algorithm
-        # encountered a fatal error. The exception text can be any string, but in this
-        # case we use the pre-built invalidSourceError method to return a standard
-        # helper text for when a source cannot be evaluated
-        if source is None:
-            raise QgsProcessingException(self.invalidSourceError(parameters, self.INPUT))
-
-        (sink, dest_id) = self.parameterAsSink(
-            parameters,
-            self.OUTPUT,
-            context,
-            source.fields(),
-            source.wkbType(),
-            source.sourceCrs()
-        )
-
-        # Send some information to the user
-        feedback.pushInfo('CRS is {}'.format(source.sourceCrs().authid()))
-
-        # If sink was not created, throw an exception to indicate that the algorithm
-        # encountered a fatal error. The exception text can be any string, but in this
-        # case we use the pre-built invalidSinkError method to return a standard
-        # helper text for when a sink cannot be evaluated
-        if sink is None:
-            raise QgsProcessingException(self.invalidSinkError(parameters, self.OUTPUT))
-
-        # Compute the number of steps to display within the progress bar and
-        # get features from source
-        total = 100.0 / source.featureCount() if source.featureCount() else 0
-        features = source.getFeatures()
-
-        for current, feature in enumerate(features):
-            # Stop the algorithm if cancel button has been clicked
-            if feedback.isCanceled():
-                break
-
-            # Add a feature in the sink
-            sink.addFeature(feature, QgsFeatureSink.FastInsert)
-
-            # Update the progress bar
-            feedback.setProgress(int(current * total))
-
-        # To run another Processing algorithm as part of this algorithm, you can use
-        # processing.run(...). Make sure you pass the current context and feedback
-        # to processing.run to ensure that all temporary layer outputs are available
-        # to the executed algorithm, and that the executed algorithm can send feedback
-        # reports to the user (and correctly handle cancellation and progress reports!)
-        if False:
-            buffered_layer = processing.run("native:buffer", {
-                'INPUT': dest_id,
-                'DISTANCE': 1.5,
-                'SEGMENTS': 5,
-                'END_CAP_STYLE': 0,
-                'JOIN_STYLE': 0,
-                'MITER_LIMIT': 2,
-                'DISSOLVE': False,
-                'OUTPUT': 'memory:'
-            }, context=context, feedback=feedback)['OUTPUT']
-
-        # Return the results of the algorithm. In this case our only result is
-        # the feature sink which contains the processed features, but some
-        # algorithms may return multiple feature sinks, calculated numeric
-        # statistics, etc. These should all be included in the returned
-        # dictionary, with keys matching the feature corresponding parameter
-        # or output names.
-        return {self.OUTPUT: dest_id}
