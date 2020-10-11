3790ba0a7448c9ecba4e9e3ffbb9d1d06110d406
Blottiere Paul <blottiere.paul@gmail.com>
2020-10-11 07:29:20 +0200

    Automatically install dependencies

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ __date__ = "2020/10/10"
 __email__ = "blottiere.paul@gmail.com"
 __license__ = "GPLv3"
 
+import subprocess
 from qgis.core import QgsApplication
 
 from .src.gui.provider.provider import LittoDynProvider
@@ -20,6 +21,7 @@ class LittoDyn(object):
     def __init__(self, iface):
         self.iface = iface
         self.provider = None
+        self.install_deps()
 
     def initProcessing(self):
         self.provider = LittoDynProvider()
@@ -30,3 +32,9 @@ class LittoDyn(object):
 
     def unload(self):
         QgsApplication.processingRegistry().removeProvider(self.provider)
+
+    def install_deps(self):
+        deps = ["numpy", "scipy", "gdal", "scikit-learn"]
+        for dep in deps:
+            cmd = ["python3", "-m", "pip", "install", dep, "--user"]
+            output = subprocess.check_output(cmd)
